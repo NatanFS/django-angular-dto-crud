e69c1c45e158fb05097dfe0b1a1b6861ad70c8b0
Natan Felipe de Freitas Souza <natanlipf@gmail.com>
2023-12-09 19:15:06 -0300

    criado serviço de pessoa

---
+++ frontend/athenas/src/app/person/person.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { Person } from './person.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  private baseURL = "http://localhost:8000";
+  private endpoint = "persons";
+
+  constructor(private httpClient: HttpClient) { 
+    
+  }
+
+  list(): Observable<Person[]> {
+    return this.httpClient.get<Person[]>(`${this.baseURL}/${this.endpoint}`);
+  }
+
+  create(person: Person): Observable<Person>{
+    return this.httpClient.post<Person>(`${this.baseURL}/${this.endpoint}/`, person);
+  }
+
+  findById(id: number): Observable<Person> {
+    return this.httpClient.get<Person>(`${this.baseURL}/${this.endpoint}/${id}/`);
+  }
+
+  update(person: Person): Observable<Person> {
+    return this.httpClient.put<Person>(`${this.baseURL}/${this.endpoint}/${person.id}/`, person);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.baseURL}/${this.endpoint}/${id}/`);
+  }
+
+  getIdealWeight(id: number): Observable<any> {
+    return this.httpClient.get(`${this.baseURL}/${this.endpoint}/${id}/ideal-weight`);
+  }
+  
+}
